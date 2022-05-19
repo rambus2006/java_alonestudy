a6221c00fe35a2298bfbdb2d01426cfa3789711f
rambus06 <w2227@e-mirim.hs.kr>
2022-05-19 15:38:44 +0900

    예제first~예제2-2(20220519)

---
+++ Behappy.java
@@ -0,0 +1,8 @@
+public class Behappy{
+	public static void main(String args[]){
+		
+		System.out.print("Be happy!");
+		Runtime.getRuntime().exec("pause");	//java시스템 명령어
+
+	}
+}
---
+++ Second_two_print_Be happy_Myfriend.java
@@ -0,0 +1,6 @@
+public class Second_two_print_Be happy_Myfriend{
+	public static void main(String args[]){
+		System.out.println("Be happy!");
+		System.out.println("My friend");
+	}
+}
